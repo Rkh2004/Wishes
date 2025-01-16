ce3892e1f54fbc8ab48eea00d628b39a020d315c
ronak <ronakkhunti50@gmail.com>
2025-01-16 10:10:22 +0530

    Changed service and controller to do CRUD operations according to specific user. Added validating user for each endpoint in service user Principal interface.

--- src/main/java/com/example/wishes/controller/WishesController.java
+++ src/main/java/com/example/wishes/controller/WishesController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,8 +29,9 @@ public class WishesController {
 
     // Get all Wish Lists
     @GetMapping
-    public ResponseEntity<List<WishListResponseDTO>> getAllWishLists() {
-        List<WishList> wishLists = wishesService.getAllWishLists();
+    public ResponseEntity<List<WishListResponseDTO>> getAllWishLists(Principal principal) {
+        String username = principal.getName();
+        List<WishList> wishLists = wishesService.getAllWishLists(username);
         List<WishListResponseDTO> wishListResponseDTOs = wishLists.stream()
                 .map(wishMapper::toWishListDTO)
                 .collect(Collectors.toList());
@@ -38,37 +40,42 @@ public class WishesController {
 
     // Get a specific Wish List by ID
     @GetMapping("/{id}")
-    public ResponseEntity<WishListResponseDTO> getWishListById(@PathVariable int id) {
-        WishList wishList = wishesService.getWishListByID(id);
+    public ResponseEntity<WishListResponseDTO> getWishListById(Principal principal, @PathVariable int id) {
+        String username = principal.getName();
+        WishList wishList = wishesService.getWishListByID(username, id);
         return ResponseEntity.ok(wishMapper.toWishListDTO(wishList));
     }
 
     // Create a new Wish List
     @PostMapping
-    public ResponseEntity<WishListResponseDTO> createWishList(@RequestBody CreateWishListDTO createWishListDTO) {
-        WishList wishList = wishesService.createWishList(createWishListDTO.getTitle());
+    public ResponseEntity<WishListResponseDTO> createWishList(Principal principal, @RequestBody CreateWishListDTO createWishListDTO) {
+        String username = principal.getName();
+        WishList wishList = wishesService.createWishList(username, createWishListDTO.getTitle());
         return new ResponseEntity<>(wishMapper.toWishListDTO(wishList), HttpStatus.CREATED);
     }
 
     // Update a Wish List
     @PutMapping("/{id}")
-    public ResponseEntity<WishListResponseDTO> updateWishList(@PathVariable int id, @RequestBody String newTitle) {
-        WishList updatedWishList = wishesService.updateWishList(id, newTitle);
+    public ResponseEntity<WishListResponseDTO> updateWishList(Principal principal, @PathVariable int id, @RequestBody String newTitle) {
+        String username = principal.getName();
+        WishList updatedWishList = wishesService.updateWishList(username, id, newTitle);
         return ResponseEntity.ok(wishMapper.toWishListDTO(updatedWishList));
     }
 
     // Delete a Wish List
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteWishList(@PathVariable int id) {
-        wishesService.deleteWishList(id);
+    public ResponseEntity<Void> deleteWishList(Principal principal, @PathVariable int id) {
+        String username = principal.getName();
+        wishesService.deleteWishList(username, id);
         return ResponseEntity.noContent().build();
     }
 
 
     // Get all Wishes in a specific Wish List
     @GetMapping("/{wishListId}/wishes")
-    public ResponseEntity<List<WishResponseDTO>> getAllWishes(@PathVariable int wishListId) {
-        List<Wish> wishes = wishesService.getAllWishes(wishListId);
+    public ResponseEntity<List<WishResponseDTO>> getAllWishes(Principal principal, @PathVariable int wishListId) {
+        String username = principal.getName();
+        List<Wish> wishes = wishesService.getAllWishes(username, wishListId);
         List<WishResponseDTO> wishResponseDTOs = wishes.stream()
                 .map(wishMapper::toWishDTO)
                 .collect(Collectors.toList());
@@ -77,35 +84,38 @@ public class WishesController {
 
     // Create a Wish in a specific Wish List
     @PostMapping("/{wishListId}/wishes")
-    public ResponseEntity<WishResponseDTO> createWish(@PathVariable int wishListId, @RequestBody CreateWishDTO newWishDTO) {
-        Wish wish = wishesService.createWish(wishListId, newWishDTO.getTitle(), false);
+    public ResponseEntity<WishResponseDTO> createWish(Principal principal, @PathVariable int wishListId, @RequestBody CreateWishDTO newWishDTO) {
+        String username = principal.getName();
+        Wish wish = wishesService.createWish(username, wishListId, newWishDTO.getTitle(), false);
         return new ResponseEntity<>(wishMapper.toWishDTO(wish), HttpStatus.CREATED);
     }
 
     // Update a Wish in a specific Wish List
     @PutMapping("{wishListId}/wishes/{wishId}")
-    public ResponseEntity<WishResponseDTO> updateWish(@PathVariable int wishListId, @PathVariable int wishId,
+    public ResponseEntity<WishResponseDTO> updateWish(Principal principal, @PathVariable int wishListId, @PathVariable int wishId,
                                                       @RequestBody CreateWishDTO updatedWish) {
-
-        Wish wish = wishesService.updateWish(wishListId, wishId, updatedWish.getTitle(), false);
+        String username = principal.getName();
+        Wish wish = wishesService.updateWish(username, wishListId, wishId, updatedWish.getTitle(), false);
         return ResponseEntity.ok(wishMapper.toWishDTO(wish));
 
     }
 
     // Mark a Wish as Completed
     @PatchMapping("/{wishListId}/wishes/{wishId}/complete")
-    public ResponseEntity<WishResponseDTO> markWishCompleted(@PathVariable int wishListId, @PathVariable int wishId) {
+    public ResponseEntity<WishResponseDTO> markWishCompleted(Principal principal, @PathVariable int wishListId, @PathVariable int wishId) {
 
-        Wish wish = wishesService.markWishCompleted(wishListId, wishId, true);
+        String username = principal.getName();
+        Wish wish = wishesService.markWishCompleted(username, wishListId, wishId, true);
         return ResponseEntity.ok(wishMapper.toWishDTO(wish));
 
     }
 
     // Delete a Wish from a specific Wish List
     @DeleteMapping("/{wishListId}/wishes/{wishId}")
-    public ResponseEntity<Void> deleteWish(@PathVariable int wishListId, @PathVariable int wishId) {
+    public ResponseEntity<Void> deleteWish(Principal principal, @PathVariable int wishListId, @PathVariable int wishId) {
 
-            wishesService.deleteWish(wishListId, wishId);
-            return ResponseEntity.noContent().build();
+        String username = principal.getName();
+        wishesService.deleteWish(username, wishListId, wishId);
+        return ResponseEntity.noContent().build();
     }
 }
